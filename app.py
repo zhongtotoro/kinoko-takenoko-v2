81b95653e31bf921b977731d0fd23de8203dfbd1
zhongtotoro <s1F102400055@iniad.org>
2025-07-21 15:38:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/api/v1/votes')
 def vote_api():
     return jsonify({'kinoko': kinoko_count, 'takenoko': takenoko_count})
+
+
 kinoko_count = 3
 takenoko_count = 5
 messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
